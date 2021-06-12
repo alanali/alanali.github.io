e4e021a5f1633b0bb7d3260f37e96149b5f927df
Alana Li <ruiruialana@gmail.com>
2021-06-11 23:09:01 -0700

    Resizing of title on scroll finalized

--- navbar.js
+++ navbar.js
@@ -4,17 +4,17 @@ var width = window.innerWidth;
 function shrink(id) {
   var size;
   if (id == "bunni") {
-    if (3.5 * width / 100 < 20) {
-      size = 20;
+    if (3.5 * width / 100 < 30) {
+      size = "30px";
     } else {
       size = "3.5vw";
     }
   }
   else if (id == "store") {
-    if (((3.5 * 100 / width) - 30) < 15) {
-      size = 15;
+    if ((3.5 * width / 100) - 30 < 15) {
+      size = "15px";
     } else {
-      size = (3.5 * width / 100) - 30;
+      size = ((3.5 * width / 100) - 30) + "px";
     }
   }
   return size;
@@ -23,17 +23,21 @@ function shrink(id) {
 function grow(id) {
   var size;
   if (id == "bunni") {
-    if (5.5 * width / 100 < 20) {
-      size = "20px+2vw";
+    if (5.5 * width / 100 < 30) {
+      size = "30px";
+    } else if (5.5 * width / 100 > 70) {
+      size = "70px";
     } else {
       size = "5.5vw";
     }
   }
   else if (id == "store") {
-    if (((5.5 * width / 100) - 30) < 15) {
-      size = (2 * width / 100) + 15;
+    if ((5.5 * width / 100) - 30 < 15) {
+      size = "15px";
+    } else if (5.5 * width / 100 > 30) {
+      size = "30px";
     } else {
-      size = (5.5 * width / 100) - 30;
+      size = (5.5 * width / 100) - 30 + "px";
     }
   }
   return size;
@@ -41,18 +45,10 @@ function grow(id) {
 
 function scrollFunction() {
   if (document.body.scrollTop > 80 || document.documentElement.scrollTop > 80) {
-    // window.alert(document.getElementById("bunni").style.fontSize);
     document.getElementById("bunni").style.fontSize = shrink("bunni");
     document.getElementById("store").style.fontSize = shrink("store");
   } else {
     document.getElementById("bunni").style.fontSize = grow("bunni");
     document.getElementById("store").style.fontSize = grow("store");
-    // if ("0.7em" > "30px") {
-    //   document.getElementById("store").style.letterSpacing = "30px";
-    // } else if ("0.7em" < "0px") {
-    //   document.getElementById("store").style.letterSpacing = "0px";
-    // } else {
-    //   document.getElementById("store").style.letterSpacing = "0.7em";
-    // }
   }
 }
