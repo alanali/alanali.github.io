3f6a659aac56fe69da6549914fc5cf8fcf158262
Alana Li <alanali@berkeley.edu>
2022-02-07 18:07:36 -0800

    Added SWE, drawing, p4

--- style.js
+++ style.js
@@ -1,3 +1,4 @@
+// -------------------- NAVBAR -------------------- //
 var titleh;
 var width = window.innerWidth;
 window.onload = function() {
@@ -73,4 +74,47 @@ function scrollFunction() {
     document.getElementById("last").style.fontSize = grow("last");
     document.getElementById("backing").style.top = "0px";
   }
+}
+
+
+// -------------------- SLIDES -------------------- //
+var slideIndex = 0;
+showSlides();
+
+function showSlides() {
+  var i;
+  var slides = document.getElementsByClassName("mySlides");
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";
+  }
+  slideIndex++;
+  if (slideIndex > slides.length) {slideIndex = 1}
+  slides[slideIndex-1].style.display = "block";
+  setTimeout(showSlides, 2000); // Change image every 2 seconds
+}
+
+// Next/previous controls
+function plusSlides(n) {
+  showSlides(slideIndex += n);
+}
+
+// Thumbnail image controls
+function currentSlide(n) {
+  showSlides(slideIndex = n);
+}
+
+function showSlides(n) {
+  var i;
+  var slides = document.getElementsByClassName("slide");
+  var dots = document.getElementsByClassName("dot");
+  if (n > slides.length) {slideIndex = 1}
+  if (n < 1) {slideIndex = slides.length}
+  for (i = 0; i < slides.length; i++) {
+      slides[i].style.display = "none";
+  }
+  for (i = 0; i < dots.length; i++) {
+      dots[i].className = dots[i].className.replace(" active", "");
+  }
+  slides[slideIndex-1].style.display = "block";
+  dots[slideIndex-1].className += " active";
 }
