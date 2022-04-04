971529fdd3581991680c430d7875399990748d88
Alana Li <alanali@berkeley.edu>
2022-04-03 22:55:36 -0700

    Removed slideshow, fixed header

--- style.js
+++ style.js
@@ -16,7 +16,7 @@ function boo(x) {
 
 function shrink(id) {
   var size;
-  if (id == "first") {
+  if (id == "title") {
     if (3.5 * width / 100 <= 30) {
       size = "25px";
     } else if (3.5 * width / 100 >= 50) {
@@ -25,7 +25,7 @@ function shrink(id) {
       size = "3.5vw";
     }
   }
-  else if (id == "last") {
+  else if (id == "subtitle") {
     if (((3.5 * width / 100) - 30) < 10) {
       size = ((3.5 * width / 95) - 15) + "px";
     } else if (((3.5 * width / 100) - 30) >= 30) {
@@ -40,7 +40,7 @@ function shrink(id) {
 
 function grow(id) {
   var size;
-  if (id == "first") {
+  if (id == "title") {
     if (5.5 * width / 100 <= 30) {
       size = "30px";
     } else if (5.5 * width / 100 >= 70) {
@@ -49,7 +49,7 @@ function grow(id) {
       size = "5.5vw";
     }
   }
-  else if (id == "last") {
+  else if (id == "subtitle") {
     if ((5.5 * width / 100) - 30 <= 15) {
       size = "15px";
     } else if ((5.5 * width / 100) - 30 > 30) {
@@ -65,56 +65,13 @@ function scrollFunction() {
   if (document.body.scrollTop > 60 || document.documentElement.scrollTop > 60) {
     document.getElementById("backing").style.top = "-10px";
     document.getElementById("backing").style.height = document.getElementById("title").clientHeight + (width / 100) + "px";
-    document.getElementById("first").style.fontSize = shrink("first");
-    document.getElementById("last").style.fontSize = shrink("last");
-    document.getElementById("cloud").width = "5px";
+    document.getElementById("title").style.fontSize = shrink("title");
+    document.getElementById("subtitle").style.fontSize = shrink("subtitle");
+    document.getElementById("cloud").height = "30px";
   } else {
     document.getElementById("backing").style.height = titleh;
-    document.getElementById("first").style.fontSize = grow("first");
-    document.getElementById("last").style.fontSize = grow("last");
+    document.getElementById("title").style.fontSize = grow("title");
+    document.getElementById("subtitle").style.fontSize = grow("subtitle");
     document.getElementById("backing").style.top = "0px";
   }
-}
-
-
-// -------------------- SLIDES -------------------- //
-var slideIndex = 0;
-showSlides();
-
-function showSlides() {
-  var i;
-  var slides = document.getElementsByClassName("mySlides");
-  for (i = 0; i < slides.length; i++) {
-    slides[i].style.display = "none";
-  }
-  slideIndex++;
-  if (slideIndex > slides.length) {slideIndex = 1}
-  slides[slideIndex-1].style.display = "block";
-  setTimeout(showSlides, 2000); // Change image every 2 seconds
-}
-
-// Next/previous controls
-function plusSlides(n) {
-  showSlides(slideIndex += n);
-}
-
-// Thumbnail image controls
-function currentSlide(n) {
-  showSlides(slideIndex = n);
-}
-
-function showSlides(n) {
-  var i;
-  var slides = document.getElementsByClassName("slide");
-  var dots = document.getElementsByClassName("dot");
-  if (n > slides.length) {slideIndex = 1}
-  if (n < 1) {slideIndex = slides.length}
-  for (i = 0; i < slides.length; i++) {
-      slides[i].style.display = "none";
-  }
-  for (i = 0; i < dots.length; i++) {
-      dots[i].className = dots[i].className.replace(" active", "");
-  }
-  slides[slideIndex-1].style.display = "block";
-  dots[slideIndex-1].className += " active";
 }
